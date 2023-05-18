06c2da2722c69455dd8249131fd3bfc0f4b99696
JosePalmax <jopalmaca@gmail.com>
2023-05-18 20:59:02 +0200

    Añadir contenido al App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Es por la mañana(1), por la tarde(2) o por la noche(3)");
+        int numero = scan.nextInt();
+        switch (numero) {
+            case 1:
+                System.out.println("Hola buenos dias");
+                break;
+            case 2:
+                System.out.println("Hola buenas tardes");
+                break;
+            case 3:
+                System.out.println("Hola buenas noches");                
+                break;
+        
+            default:
+                break;
+        }
+        scan.close();
     }
 }
